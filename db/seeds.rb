ae9fb7786a0a145f0e504ebe291074abde00afc1
pittsblake <pittsblake1@gmail.com>
2017-10-26 16:47:49 -0400

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Fridge.destroy_all
+
+maytag = Fridge.create(location: 'kitchen', brand: 'MayTag', size: 200, has_food: true, has_drink: true)
+ge = Fridge.create(location: 'garage', brand: 'GE', size: 200, has_food: true, has_drink: true)
+
+Food.create(fridge_id: maytag.id,name: "Pizza",weight: 10, vegan: true,time_stamp: Date.today) 
+Food.create(fridge_id:ge.id, name: "Fruit",weight: 10, vegan: true,time_stamp: Date.today) 
+
+Drink.create(fridge_id: maytag.id, name: "Beer",size: 40, alchoholic: true)
+Drink.create(fridge_id:ge.id, name: "milk",size: 40, alchoholic: true)
--- lib/fridge_app.rb
+++ lib/fridge_app.rb
@@ -1,4 +1,4 @@
 require_relative '../config/environment'
-require_relative '../app/model/fridge'
+require_relative '../app/models/fridge'
 
 puts 'test'
